effe0920cc701d428bb80d6ea512d5312d9ff872
vishal.Alok <you@example.com>
2021-10-15 09:45:53 +0530

    Add project files.

---
+++ enum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace enumPractice
+{
+    public enum Days { Sat = 1, Sun, Mon= 0, Tue, Wed, Thu, Fri };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Days.Sun + " is day " + (int)Days.Sun);
+            Console.WriteLine(Days.Tue + " is day " + (int)Days.Tue);
+        }
+    }
+}
